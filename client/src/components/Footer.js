a53a7eda1242e42c870ae00418c82435fd6a5697
Elaine Liao <elaine_liao@sbcglobal.net>
2022-09-29 20:38:04 -0700

    created components folder and files

---
+++ client/src/components/Footer.js
@@ -0,0 +1,11 @@
+import React from 'react';
+
+function Footer() {   
+    return (
+    <footer className="footer">
+        <h2>FOOTER</h2>
+    </footer>
+    )
+}
+
+export default Footer;
---
+++ client/src/components/Login.js
@@ -0,0 +1,11 @@
+import React from 'react';
+
+function Login() {   
+    return (
+    <div className="loginPage">
+        <h2>LOGIN</h2>
+    </div>
+    )
+}
+
+export default Login;
