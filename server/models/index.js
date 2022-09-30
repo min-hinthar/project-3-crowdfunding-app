4bf0c12d3f5df571707147a94aed0d882ab0c69a
AlexaP2022 <alexapunzalan@gmail.com>
2022-09-29 20:27:57 -0700

    added code for models, schema, seeds files, and config file

---
+++ server/models/index.js
@@ -0,0 +1,4 @@
+const Project = require('./project');
+const User = require('./user');
+
+module.exports = { Project, User };
---
+++ server/schemas/typeDefs.js
@@ -0,0 +1,49 @@
+const { gql } = require('apollo-server-express');
+
+const typeDefs = gql`
+  type Project {
+    _id: ID
+    name: String
+    description: String
+    pledgeGoal: Number
+    createdAt: Date
+    assets: [Asset]!
+  }
+  type User {
+    _id: ID
+    username: String
+    email: String
+    password: String
+    projects: [Project]!
+    assets: [Asset]!
+  }
+  type Asset {
+    _id: ID
+    title: String
+    description: String
+    price: Number
+    createdAt: Date
+  }
+  type Auth {
+    token: ID!
+    user: User
+  }
+  type Query {
+    users: [User]
+    user(username: String!): User
+    projects: [Project]!
+    project(projectId: ID!): Project
+}
+
+  type Mutation {
+    # Set up mutations to handle creating a profile or logging into a profile and return Auth type
+    addUser (username: String!, email: String!, password: String!): Auth
+    addAsset (title: String!, description: String!, createdAt: Date!): Asset
+    addProject(name: String!, description: String!, pledgeGoal: Number!): Project
+    login(email: String!, password: String!): Auth
+    removeProject(projectId: ID!): Project
+    removeAsset(assetId: ID!): Asset
+  }
+`;
+
+module.exports = typeDefs;
