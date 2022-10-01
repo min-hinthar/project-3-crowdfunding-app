b4d9fcf4653862db00122f3061d5f473502c6293
AlexaP2022 <alexapunzalan@gmail.com>
2022-10-01 09:59:12 -0700

    added mutations for addUser and login resolvers and tested launching application from root using npm run develop

---
+++ server/schemas/resolvers.js
@@ -0,0 +1,32 @@
+const { AuthenticationError } = require('apollo-server-express');
+const { Profile, User } = require('../models');
+const { signToken } = require('../utils/authenticate');
+
+const resolvers = {
+    Mutation: {
+        addUser: async (parent, { username, email, password }) => {
+            const user = await User.create({ username, email, password });
+            const token = signToken(user);
+      
+            return { token, user };
+          },
+        login: async (parent, { email, password }) => {
+            const user = await User.findOne({ email });
+      
+            if (!user) {
+              throw new AuthenticationError('No user with this email found!');
+            }
+      
+            const correctPw = await user.isCorrectPassword(password);
+      
+            if (!correctPw) {
+              throw new AuthenticationError('Incorrect password!');
+            }
+      
+            const token = signToken(user);
+            return { token, user };
+          },
+    }
+}
+
+module.exports = resolvers;
