523b70cfaf63626233f467f9cb1e90a2a8736e39
min-hinthar <102896221+min-hinthar@users.noreply.github.com>
2022-10-06 17:52:54 -0700

    strip integration debugging

--- server/server.js
+++ server/server.js
@@ -52,3 +52,32 @@ const startApolloServer = async ( resolvers, typeDefs) => {
 
 // call startApolloServer using resolvers and typeDefs to run server on PORT
 startApolloServer(resolvers, typeDefs);
+
+
+// This is a public sample test API key.
+// Don’t submit any personally identifiable information in requests made with this key.
+// Sign in to see your own test API key embedded in code samples.
+import publishableKey from './client/src/.env';
+const stripe = require('stripe')(publishableKey);
+const express = require('express');
+
+const YOUR_DOMAIN = 'http://localhost:4242';
+
+app.post('/create-checkout-session', async (req, res) => {
+  const session = await stripe.checkout.sessions.create({
+    line_items: [
+      {
+        // Provide the exact Price ID (for example, pr_1234) of the product you want to sell
+        price: '{{PRICE_ID}}',
+        quantity: 1,
+      },
+    ],
+    mode: 'payment',
+    success_url: `${YOUR_DOMAIN}?success=true`,
+    cancel_url: `${YOUR_DOMAIN}?canceled=true`,
+  });
+
+  res.redirect(303, session.url);
+});
+
+app.listen(4242, () => console.log('Running on port 4242'));
